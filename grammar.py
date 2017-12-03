0b78199793eaee208ba0949647980a25fa79bb44
aphedges <aphedges1@gmail.com>
2017-12-02 20:30:56 -0500

    Split main method into functions for modularity

--- grammar.py
+++ grammar.py
@@ -1,22 +1,80 @@
 """
 Author: Alex Hedges
 Written for Python 3.6.3.
+Requires NLTK and its subpackage averaged_perceptron_tagger
 """
 
 import argparse
+from itertools import accumulate
 import os
 import random
-from itertools import accumulate
+import re
+import string
 
 import nltk
 from nltk import pos_tag
+from nltk.grammar import Nonterminal
+from nltk.grammar import PCFG
+from nltk.grammar import Production
+from nltk.parse import ViterbiParser
 
 from main import read_data
 
+REMOVE_DIGITS_REGEX = re.compile(r'^(.*?)([=\-]\d+)*$')
+REMOVE_SUFFIX_REGEX = re.compile(r'^(.*?)((?:-|=).*)?$')
+
+def simplify_nonterminal(nonterm: Nonterminal) -> Nonterminal:
+	return Nonterminal(REMOVE_SUFFIX_REGEX.fullmatch(nonterm.symbol()).group(1))
+
+def valid_nonterminal(nonterm: Nonterminal) -> bool:
+	return nonterm.symbol()[0] in string.ascii_letters
+
+def create_grammar() -> PCFG:
+	# 21,763 productions with word terminals
+	# 8,028 productions with pos terminals
+	# 6,275 productions with nonterminals without digits
+	# 5,402 productions with nonterminals without punctuation
+	# 2,972 productions with nonterminals without suffixes
+	# 707 nonterminals
+	# 190 nonterminals without digit labels
+	# 180 nonterminals without punctuation
+	# 63 nonterminals without suffixes
+	productions = []
+	start_symbol = Nonterminal('S')
+	for tree in nltk.corpus.treebank.parsed_sents():
+		for production in tree.productions():
+			if not valid_nonterminal(production.lhs()):
+				continue
+			if isinstance(production.rhs()[0], Nonterminal):
+				lhs = simplify_nonterminal(production.lhs())
+				rhs = tuple(simplify_nonterminal(t) for t in production.rhs() if valid_nonterminal(t))
+				productions.append(Production(lhs, rhs))
+			else:
+				simplified = simplify_nonterminal(production.lhs())
+				productions.append(Production(simplified, (simplified.symbol(),)))
+
+	grammar = nltk.induce_pcfg(start_symbol, productions)
+	#print(grammar.productions())
+	print(len(grammar.productions()))
+	nonterminals = set(prod.lhs() for prod in grammar.productions())
+	print(sorted(nonterminals))
+	print(len(nonterminals))
+	return grammar
+
+def evaluate_sentence(sentence: string, grammar: PCFG):
+	sentence = sentence.split()
+	print(sentence, flush=True)
+	pos = [pos for word, pos in pos_tag(sentence)]
+	print(pos, flush=True)
+	parser = ViterbiParser(grammar, trace=0)
+	for line in accumulate(pos, lambda total, token: total + ' ' + token):
+		line = line.split()
+		print(line)
+		print([tree.prob() for tree in list(parser.parse(line))], flush=True)
+
 def main():
-	"""Reads in transcript data and tests the turn-taking detector"""
-	parser = argparse.ArgumentParser(description=
-			'Runs turn-taking detector.')
+	"""Tests the syntax analyzing portions of the turn-taking detector"""
+	parser = argparse.ArgumentParser(description='Runs turn-taking detector.')
 	parser.add_argument('transcript_file', help='File containing the collated utterance information.')
 	args = vars(parser.parse_args())
 	transcript_file = args['transcript_file']
@@ -31,35 +89,12 @@ def main():
 	random.shuffle(sentences)
 	print(sentences[:10])
 
-	# 21,763 with word terminals
-	# 8,028 with pos terminals
-	productions = []
-	start_symbol = nltk.Nonterminal('S')
-	for tree in nltk.corpus.treebank.parsed_sents():
-		#print(tree.productions())
-		for production in tree.productions():
-			if isinstance(production.rhs()[0], nltk.grammar.Nonterminal):
-				productions.append(production)
-			else:
-				productions.append(nltk.grammar.Production(production.lhs(), (production.lhs().symbol(),)))
-		#print(productions)
-		#exit()
-	grammar = nltk.induce_pcfg(start_symbol, productions)
-	print(len(grammar.productions()))
-	#print(grammar.productions()[1000:1020])
-	#print(grammar.check_coverage(['NN', 'VBP']))
-	sentence = 'Okay'.split()
+	grammar = create_grammar()
+	sentence = 'that\'s where the real sticking point comes in'
 	# "I went to the house"
 	# "The house I did go"
 	# "that's where the real sticking point comes in"
-	print(sentence, flush=True)
-	pos = [pos for word, pos in pos_tag(sentence)]
-	print(pos, flush=True)
-	for line in accumulate(pos, lambda total, token: total + ' ' + token):
-		line = line.split()
-		print(line)
-		print([tree.prob() for tree in
-			list(nltk.parse.ViterbiParser(grammar, trace=0).parse(line))], flush=True)
+	evaluate_sentence(sentence, grammar)
 
 if __name__ == '__main__':
 	main()
